8e5f34b8b1a6042972745b65f3c63cf9b5bc0600
Ibrahim Dauda <dabdulxyz@gmail.com>
2022-03-20 21:33:36 +0100

    Updated Projects Sction

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,14 +8,19 @@ const Hero = () => (
   <Section row nopadding>
     <LeftSection>
       <SectionTitle main center>
-      HeY, My Name is Ibrahim Dauda ;)<br />
-      I'm a Software Developer, <br />I build Mobile & Web Application
+        HeY, My Name is Ibrahim Dauda ;)<br />
+        I'm a FrontEnd Developer,
+        {/* <br />I build Mobile & Web Application */}
       </SectionTitle>
       <SectionText>
-      I’ve always been someone who has both a creative and logical MindSet,
+        Frontend developer with a Creative mindset, focusing towards the
+        development of applications to deliver solutions with a creative and customer
+        first approach. I am open-minded and focused on never-ending professional
+        self-improvement and highly interested in working as a Frontend Developer
+        {/* I’ve always been someone who has both a creative and logical MindSet,
       I am open-minded and focused on never-ending professional self-improvement and highly
        interested in working as a Full-Stack Web or Mobile App Developer using JavaScript Technologies 
-       @ any Reputable Tech Company.
+       @ any Reputable Tech Company. */}
       </SectionText>
       <Button>Learn More</Button>
     </LeftSection>
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { DiFirebase, DiReact } from 'react-icons/di';
 import { FaReacteurope, } from 'react-icons/fa';
-import { SiJavascript, SiExpertsexchange, SiMongodb, SiMysql, SiRedux} from 'react-icons/si';
+import { SiJavascript, SiExpertsexchange, SiMongodb, SiMysql, SiRedux, SiTailwindcss, SiTypescript, SiGraphql } from 'react-icons/si';
 import { IoLogoNodejs } from 'react-icons/io';
+import { BiSkipNextCircle } from 'react-icons/bi';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
-const Technologies = () =>  (
+const Technologies = () => (
   <Section id="tech">
     <SectionDivider />
     <SectionTitle>Technologies I USed</SectionTitle>
@@ -15,55 +16,73 @@ const Technologies = () =>  (
     </SectionText>
     <List>
       <ListItem>
-        <SiJavascript size="5rem" style={{ color: "#00d4ff"}} />
+        <SiJavascript size="5rem" style={{ color: "#00d4ff" }} />
         <ListContainer>
           <ListTitle>JavaScript</ListTitle>
         </ListContainer>
       </ListItem>
       <ListItem>
-        <DiReact size="5rem" style={{ color: "#00d4ff"}} />
+        <SiTypescript size="5rem" style={{ color: "#00d4ff" }} />
         <ListContainer>
-          <ListTitle>React JS</ListTitle>
+          <ListTitle>TypeScript</ListTitle>
         </ListContainer>
       </ListItem>
       <ListItem>
-        <FaReacteurope size="5rem" style={{ color: "#00d4ff"}} />
+        <BiSkipNextCircle size="5rem" style={{ color: "#00d4ff" }} />
         <ListContainer>
-          <ListTitle>React Native</ListTitle>
+          <ListTitle>NextJS</ListTitle>
         </ListContainer>
       </ListItem>
       <ListItem>
-        <SiRedux size="5rem" style={{ color: "#00d4ff"}} />
+        <SiGraphql size="5rem" style={{ color: "#00d4ff" }} />
+        <ListContainer>
+          <ListTitle>GraphQL</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiRedux size="5rem" style={{ color: "#00d4ff" }} />
         <ListContainer>
           <ListTitle>Redux</ListTitle>
         </ListContainer>
       </ListItem>
       <ListItem>
-        <IoLogoNodejs size="5rem" style={{ color: "#00d4ff"}} />
+        <SiTailwindcss size="5rem" style={{ color: "#00d4ff" }} />
+        <ListContainer>
+          <ListTitle>TailwindCSS</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <FaReacteurope size="5rem" style={{ color: "#00d4ff" }} />
+        <ListContainer>
+          <ListTitle>React Native</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <IoLogoNodejs size="5rem" style={{ color: "#00d4ff" }} />
         <ListContainer>
           <ListTitle>Node Js</ListTitle>
         </ListContainer>
       </ListItem>
-       <ListItem>
-        <SiExpertsexchange size="5rem" style={{ color: "#00d4ff"}} />
+      <ListItem>
+        <SiExpertsexchange size="5rem" style={{ color: "#00d4ff" }} />
         <ListContainer>
           <ListTitle>Express JS</ListTitle>
         </ListContainer>
       </ListItem>
       <ListItem>
-        <SiMongodb size="5rem" style={{ color: "#00d4ff"}} />
+        <SiMongodb size="5rem" style={{ color: "#00d4ff" }} />
         <ListContainer>
           <ListTitle>MongoDB</ListTitle>
         </ListContainer>
       </ListItem>
       <ListItem>
-        <SiMysql size="5rem" style={{ color: "#00d4ff"}} />
+        <SiMysql size="5rem" style={{ color: "#00d4ff" }} />
         <ListContainer>
           <ListTitle>MySQL</ListTitle>
         </ListContainer>
       </ListItem>
-     <ListItem>
-        <DiFirebase size="5rem" style={{ color: "#00d4ff"}} />
+      <ListItem>
+        <DiFirebase size="5rem" style={{ color: "#00d4ff" }} />
         <ListContainer>
           <ListTitle>FireBaseDB</ListTitle>
         </ListContainer>
